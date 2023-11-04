29af283ffd866b8ceb4f0a93328f86e7d3d5bab4
solomonadzape95 <adzapesolomon@gmail.com>
2023-11-04 08:16:54 +0100

    Landing page demo

---
+++ script.js
@@ -0,0 +1,31 @@
+// let user_range = document.querySelector("#indicator-one");
+// let user_num = document.getElementById("user-num");
+
+// function checkStats(category, display) {
+//   let goal;
+//   if (category === user_num) {
+//     goal = 1000000;
+//   } else if (category === unit_num) {
+//     goal = 100000;
+//   } else if (category === earnings) {
+//     goal = 1000000;
+//   } else if (category === mails) {
+//     goal = 100000;
+//   }
+// let val = Number(category.textContent)
+//   let width = (val/ goal) * 100;
+//   console.log();
+  
+//   display.style.width = width;
+// }
+
+// checkStats(user_num, user_range);
+
+let menu = document.getElementById('menu-toggle')
+
+if(menu.checked){
+    console.log(true);
+}else{
+    console.log(false);
+    
+}
